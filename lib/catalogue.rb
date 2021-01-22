37aea20bdab4c5ee8ead0bf9ede0bb5cdf5844c4
Prasanna Joshi <prasanna.joshi@envato.com>
2021-01-23 01:33:22 +1100

    Hard-code the catalogue with products

--- lib/catalogue.rb
+++ lib/catalogue.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
 class Catalogue
+  PRODUCTS = {
+    "R12" => {
+      5 => 699,
+      10 => 1299,
+    },
+    "L09" => {
+      3 => 995,
+      6 => 1695,
+      9 => 2495,
+    },
+    "T58" => {
+      3 => 595,
+      5 => 995,
+      9 => 1699,
+    },
+  }
+  private_constant :PRODUCTS
+
   def self.bundles
+    PRODUCTS
   end
 end
