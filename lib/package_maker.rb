60aea9dd182a6aa166c6745b8a7eb362e02644c5
Prasanna Joshi <prasanna.joshi@envato.com>
2021-01-25 02:06:15 +1100

    Refactor creation of an optimised package

--- lib/package_maker.rb
+++ lib/package_maker.rb
@@ -13,6 +13,18 @@ class PackageMaker
   def make
     return {} if ordered_quantity < available_bundle_sizes.last
 
+    package = create_optimised_package
+
+    return {} if package_incomplete?(package)
+
+    package_grouped_by_sizes(package)
+  end
+
+  private
+
+  attr_reader :ordered_quantity, :available_bundle_sizes
+
+  def create_optimised_package
     package = []
 
     while available_bundle_sizes.any?
@@ -23,29 +35,22 @@ class PackageMaker
         count.times { package << bundle_size }
       end
 
-      if package_incomplete?(package)
-        removed_available_bundle_size = available_bundle_sizes.shift
-        removed_bundle_size_from_package = package.pop
+      unless package_incomplete?(package)
+        return package
+      end
 
-        until removed_bundle_size_from_package == removed_available_bundle_size || package.empty?
-          # Remove packaged bundles until the last remaining packaged bundle size
-          # is the same as the one that will not be considered for packaging any more,
-          # or there is no more available bundle size remaining
-          removed_bundle_size_from_package = package.pop
-        end
-      else
-        break
+      removed_available_bundle_size = available_bundle_sizes.shift
+      removed_bundle_size_from_package = package.pop
+      until removed_bundle_size_from_package == removed_available_bundle_size || package.empty?
+        # Remove packaged bundles until the last remaining packaged bundle size
+        # is the same as the one that will not be considered for packaging any more,
+        # or there is no more available bundle size remaining
+        removed_bundle_size_from_package = package.pop
       end
     end
 
-    return {} if package_incomplete?(package)
-
-    package_grouped_by_sizes(package)
+    package
   end
-
-  private
-
-  attr_reader :ordered_quantity, :available_bundle_sizes
 
   def package_incomplete?(package)
     package.sum != ordered_quantity
